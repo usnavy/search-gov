1f9e28f2dea533445a01d412859a6c66a2223443
Moth <martha@rrsoft.co>
2017-04-21 11:25:22 -0700

    seed files

---
+++ db/seeds/cfpb/cfpb.rb
@@ -0,0 +1,10 @@
+cfpb = Affiliate.create!(name: "cfpb_#{Time.now.to_i}", display_name: "cfpb_#{Time.now.to_i}")
+puts "Created affiliate: #{cfpb.name}"
+
+urls = CSV.read("#{Rails.root}/db/seeds/cfpb/cfpb_top_urls.csv").map(&:first)
+
+urls.each { |url| cfpb.indexed_documents.create(url: url) }
+puts "Created #{cfpb.indexed_documents.count} indexed documents"
+
+puts "Fetching docs...."
+cfpb.indexed_documents.each { |doc| doc.fetch }
