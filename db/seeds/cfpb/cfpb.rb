94e235c247c271d3e67ad3cd88ceba91b53e5022
Moth <martha@rrsoft.co>
2017-04-27 11:38:46 -0700

    list without query params

--- db/seeds/cfpb/cfpb.rb
+++ db/seeds/cfpb/cfpb.rb
@@ -1,10 +1,27 @@
+ActiveRecord::Base.logger = nil
+
 cfpb = Affiliate.create!(name: "cfpb_#{Time.now.to_i}", display_name: "cfpb_#{Time.now.to_i}", gets_blended_results: true)
 puts "Created affiliate: #{cfpb.name}"
 
 urls = CSV.read("#{Rails.root}/db/seeds/cfpb/cfpb_top_urls.csv").map(&:first)
 
-urls.each { |url| cfpb.indexed_documents.create(url: url) }
+bar = ProgressBar.create(total: urls.count, title: 'Creating docs')
+
+urls.each do |url|
+ begin
+   cfpb.indexed_documents.create!(url: url)
+ rescue StandardError => e
+   puts "Unable to create doc for #{url}\nError: #{e}"
+ ensure
+   bar.increment
+ end
+end
+
 puts "Created #{cfpb.indexed_documents.count} indexed documents"
 
-puts "Fetching docs...."
-cfpb.indexed_documents.each { |doc| doc.fetch }
+bar = ProgressBar.create(total: cfpb.indexed_documents.count, title: 'Fetching docs')
+
+cfpb.indexed_documents.each do |doc| 
+  doc.fetch
+  bar.increment
+end
