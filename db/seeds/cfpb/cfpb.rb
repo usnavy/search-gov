e6cbcc1241a14ba00f508135bed2ad564bff7680
Moth <martha@rrsoft.co>
2017-04-27 12:29:34 -0700

    remove progressbar

--- db/seeds/cfpb/cfpb.rb
+++ db/seeds/cfpb/cfpb.rb
@@ -5,23 +5,17 @@ puts "Created affiliate: #{cfpb.name}"
 
 urls = CSV.read("#{Rails.root}/db/seeds/cfpb/cfpb_top_urls.csv").map(&:first)
 
-bar = ProgressBar.create(total: urls.count, title: 'Creating docs')
 
 urls.each do |url|
  begin
    cfpb.indexed_documents.create!(url: url)
  rescue StandardError => e
    puts "Unable to create doc for #{url}\nError: #{e}"
- ensure
-   bar.increment
  end
 end
 
 puts "Created #{cfpb.indexed_documents.count} indexed documents"
 
-bar = ProgressBar.create(total: cfpb.indexed_documents.count, title: 'Fetching docs')
-
 cfpb.indexed_documents.each do |doc| 
   doc.fetch
-  bar.increment
 end
